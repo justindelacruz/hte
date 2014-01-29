f738d17d71f54625c4ed673415aea119d615909f
Justin Dela Cruz <justindelacruz@server.fake>
2014-01-29 00:10:11 -0800

    Add logging

--- updateAssets.sh
+++ updateAssets.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 php bin/composer.phar update hte/asset-bundle --no-dev --optimize-autoloader --prefer-dist
-
+NOW="`date`"
+echo "$NOW" >> updateAssets.log
+echo "DONE"
--- updateCache.sh
+++ updateCache.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 php app/console assets:install web
 php app/console cache:clear --env=prod --no-debug
+NOW="`date`"
+echo "$NOW" >> updateCache.log
